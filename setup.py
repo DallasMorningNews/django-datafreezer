6bacef12b1dfc6f8996d34570627b24ea9587055
thirawr <tallyndavis@comcast.net>
2016-09-01 18:07:40 -0500

    Removed req.in mysql req, added pip files

---
+++ setup.py
@@ -0,0 +1,47 @@
+# Imports from python.  # NOQA
+from setuptools import setup, find_packages  # NOQA
+import os
+
+
+DESCRIPTION = 'Data storage Django web application.'
+
+REPO_URL = 'https://github.com/dallasmorningnews/django-datafreezer'
+
+VERSION = '0.0.1'
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='django-datafreezer',
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=read('readme.md'),
+    url=REPO_URL,
+    download_url=REPO_URL + '/tarball/' + VERSION,
+    author='Tyler Allyn Davis, The Dallas Morning News',
+    author_email='tallyndavis@comcast.net',
+    license='MIT',
+    packages=find_packages(exclude=['demo']),
+    include_package_data=True,
+    keywords=['django', 'csv', 'data', 'storage', 'journalism', 'javascript'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3',
+        # 'Programming Language :: Python :: 3.1',
+        # 'Programming Language :: Python :: 3.2',
+        # 'Programming Language :: Python :: 3.3',
+    ],
+    zip_safe=False,
+)
